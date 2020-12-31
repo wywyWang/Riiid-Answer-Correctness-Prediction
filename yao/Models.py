149f1cd15daf64d0a5181352d17f5ee5e340f947
yao <sf163864@gmail.com>
2020-12-31 14:21:03 +0800

    split notebook code to .py code, refer from karl

---
+++ yao/Models.py
@@ -0,0 +1,163 @@
+import torch.nn as nn
+import numpy as np
+import torch
+from sklearn.metrics import roc_auc_score
+
+MAX_SEQ = 180
+DROPOUT_RATE = 0.2
+
+
+class FFN(nn.Module):
+    def __init__(self, state_size=200, forward_expansion=1, bn_size=MAX_SEQ-1, dropout=0.2):
+        super(FFN, self).__init__()
+        self.state_size = state_size
+        
+        self.lr1 = nn.Linear(state_size, forward_expansion * state_size)
+        self.relu = nn.ReLU()
+        self.bn = nn.BatchNorm1d(bn_size)
+        self.lr2 = nn.Linear(forward_expansion * state_size, state_size)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x):
+        x = self.relu(self.lr1(x))
+        x = self.bn(x)
+        x = self.lr2(x)
+        return self.dropout(x)
+
+
+def future_mask(seq_length):
+    future_mask = (np.triu(np.ones([seq_length, seq_length]), k = 1)).astype('bool')
+    return torch.from_numpy(future_mask)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_dim, heads=8, dropout=DROPOUT_RATE, forward_expansion=1):
+        super(TransformerBlock, self).__init__()
+        self.multi_att = nn.MultiheadAttention(embed_dim=embed_dim, num_heads=heads, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
+        self.layer_normal = nn.LayerNorm(embed_dim)
+        self.ffn = FFN(embed_dim, forward_expansion = forward_expansion, dropout=dropout)
+        self.layer_normal_2 = nn.LayerNorm(embed_dim)
+        
+
+    def forward(self, value, key, query, att_mask):
+        att_output, att_weight = self.multi_att(value, key, query, attn_mask=att_mask)
+        att_output = self.dropout(self.layer_normal(att_output + value))
+        att_output = att_output.permute(1, 0, 2) # att_output: [s_len, bs, embed] => [bs, s_len, embed]
+        x = self.ffn(att_output)
+        x = self.dropout(self.layer_normal_2(x + att_output))
+        return x.squeeze(-1), att_weight
+    
+
+class Encoder(nn.Module):
+    def __init__(self, n_skill, max_seq=100, embed_dim=128, dropout=DROPOUT_RATE, forward_expansion=1, num_layers=1, heads = 8):
+        super(Encoder, self).__init__()
+        self.n_skill, self.embed_dim = n_skill, embed_dim
+        self.embedding = nn.Embedding(2 * n_skill + 1, embed_dim)
+        self.pos_embedding = nn.Embedding(max_seq - 1, embed_dim)
+        self.e_embedding = nn.Embedding(n_skill+1, embed_dim)
+        self.layers = nn.ModuleList([TransformerBlock(embed_dim, forward_expansion = forward_expansion) for _ in range(num_layers)])
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x, question_ids):
+        device = x.device
+        x = self.embedding(x)
+        pos_id = torch.arange(x.size(1)).unsqueeze(0).to(device)
+        pos_x = self.pos_embedding(pos_id)
+        x = self.dropout(x + pos_x)
+        x = x.permute(1, 0, 2) # x: [bs, s_len, embed] => [s_len, bs, embed]
+        e = self.e_embedding(question_ids)
+        e = e.permute(1, 0, 2)
+        for layer in self.layers:
+            att_mask = future_mask(e.size(0)).to(device)
+            x, att_weight = layer(e, x, x, att_mask=att_mask)
+            x = x.permute(1, 0, 2)
+        x = x.permute(1, 0, 2)
+        return x, att_weight
+
+
+class SAKTModel(nn.Module):
+    def __init__(self, n_skill, max_seq=100, embed_dim=128, dropout=DROPOUT_RATE, forward_expansion = 1, enc_layers=1, heads = 8):
+        super(SAKTModel, self).__init__()
+        self.encoder = Encoder(n_skill, max_seq, embed_dim, dropout, forward_expansion, num_layers=enc_layers)
+        self.pred = nn.Linear(embed_dim, 1)
+        
+    def forward(self, x, question_ids):
+        x, att_weight = self.encoder(x, question_ids)
+        x = self.pred(x)
+        return x.squeeze(-1), att_weight
+
+
+def train_fn(model, dataloader, optimizer, scheduler, criterion, device="cpu"):
+    model.train()
+
+    train_loss = []
+    num_corrects = 0
+    num_total = 0
+    labels = []
+    outs = []
+
+    for item in dataloader:
+        x = item[0].to(device).long()
+        target_id = item[1].to(device).long()
+        label = item[2].to(device).float()
+        target_mask = (target_id != 0)
+
+        optimizer.zero_grad()
+        output, _, = model(x, target_id)
+        loss = criterion(output, label)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        train_loss.append(loss.item())
+
+        output = torch.masked_select(output, target_mask)
+        label = torch.masked_select(label, target_mask)
+        pred = (torch.sigmoid(output) >= 0.5).long()
+        
+        num_corrects += (pred == label).sum().item()
+        num_total += len(label)
+
+        labels.extend(label.view(-1).data.cpu().numpy())
+        outs.extend(output.view(-1).data.cpu().numpy())
+
+    acc = num_corrects / num_total
+    auc = roc_auc_score(labels, outs)
+    loss = np.mean(train_loss)
+
+    return loss, acc, auc
+
+def valid_fn(model, dataloader, criterion, device="cpu"):
+    model.eval()
+
+    valid_loss = []
+    num_corrects = 0
+    num_total = 0
+    labels = []
+    outs = []
+
+    for item in dataloader:
+        x = item[0].to(device).long()
+        target_id = item[1].to(device).long()
+        label = item[2].to(device).float()
+        target_mask = (target_id != 0)
+
+        output, _, = model(x, target_id)
+        loss = criterion(output, label)
+        valid_loss.append(loss.item())
+
+        output = torch.masked_select(output, target_mask)
+        label = torch.masked_select(label, target_mask)
+        pred = (torch.sigmoid(output) >= 0.5).long()
+        
+        num_corrects += (pred == label).sum().item()
+        num_total += len(label)
+
+        labels.extend(label.view(-1).data.cpu().numpy())
+        outs.extend(output.view(-1).data.cpu().numpy())
+
+    acc = num_corrects / num_total
+    auc = roc_auc_score(labels, outs)
+    loss = np.mean(valid_loss)
+
+    return loss, acc, auc
---
+++ yao/kaggle_sakt.py
@@ -0,0 +1,116 @@
+import gc
+import psutil
+import joblib
+import random
+import time
+import os
+from tqdm import tqdm
+
+import numpy as np
+import pandas as pd
+import datatable as dt
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+import Dataset as DS
+import Models as MD
+#######################################
+seed_value = 42
+torch.manual_seed(seed_value)
+torch.cuda.manual_seed(seed_value)
+torch.cuda.manual_seed_all(seed_value) # gpu vars
+# torch.backends.cudnn.deterministic = True  #needed
+# torch.backends.cudnn.benchmark = False
+#######################################
+TRAIN_SAMPLES = 320000
+MAX_SEQ = 180
+MIN_SAMPLES = 5
+EMBED_DIM = 128
+DROPOUT_RATE = 0.2
+LEARNING_RATE = 2e-3
+MAX_LEARNING_RATE = 2e-3
+EPOCHS = 10
+TRAIN_BATCH_SIZE = 64
+ACCEPTED_USER_CONTENT_SIZE = 4
+#######################################
+
+# Create folder
+folder = "model/"  + time.strftime("%Y%m%d-%H%M%S")
+os.makedirs(folder, exist_ok=True)
+
+## Load Data
+dtypes = {'timestamp': 'int64', 'user_id': 'int32' ,'content_id': 'int16','content_type_id': 'int8','answered_correctly':'int8'}
+train_df = dt.fread('./riiid-test-answer-prediction/train.csv', columns=set(dtypes.keys())).to_pandas()
+for col, dtype in dtypes.items():
+    train_df[col] = train_df[col].astype(dtype)
+train_df = train_df[train_df.content_type_id == False]
+train_df = train_df.sort_values(['timestamp'], ascending=True)
+train_df.reset_index(drop=True, inplace=True)
+
+## Preprocess
+skills = train_df["content_id"].unique()
+joblib.dump(skills, folder + "/skills.pkl.zip")
+n_skill = len(skills)
+print("number skills", len(skills))
+
+group = train_df[['user_id', 'content_id', 'answered_correctly']].groupby('user_id').apply(lambda r: (
+            r['content_id'].values,
+            r['answered_correctly'].values))
+
+joblib.dump(group, folder + "/group.pkl.zip")
+del train_df
+gc.collect()
+
+train_indexes = list(group.index)[:TRAIN_SAMPLES]
+valid_indexes = list(group.index)[TRAIN_SAMPLES:]
+train_group = group[group.index.isin(train_indexes)]
+valid_group = group[group.index.isin(valid_indexes)]
+del group, train_indexes, valid_indexes
+print(len(train_group), len(valid_group))
+
+# Prepare training and validation data
+train_dataset = DS.SAKTDataset(train_group, n_skill, max_seq=MAX_SEQ)
+train_dataloader = DataLoader(train_dataset, batch_size=TRAIN_BATCH_SIZE, shuffle=True, num_workers=8)
+del train_group
+valid_dataset = DS.SAKTDataset(valid_group, n_skill, max_seq=MAX_SEQ)
+valid_dataloader = DataLoader(valid_dataset, batch_size=TRAIN_BATCH_SIZE, shuffle=False, num_workers=8)
+del valid_group
+
+#############
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = MD.SAKTModel(n_skill, max_seq=MAX_SEQ, embed_dim=EMBED_DIM, dropout=DROPOUT_RATE, enc_layers=1)
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+criterion = nn.BCEWithLogitsLoss()
+scheduler = torch.optim.lr_scheduler.OneCycleLR(
+    optimizer, max_lr=MAX_LEARNING_RATE, steps_per_epoch=len(train_dataloader), epochs=EPOCHS
+)
+
+model.to(device)
+criterion.to(device)
+
+#############
+
+best_auc = 0
+max_steps = 3
+step = 0
+for epoch in tqdm(range(EPOCHS)):
+    loss, acc, auc = MD.train_fn(model, train_dataloader, optimizer, scheduler, criterion, device)
+    print("[epoch - {}/{}] [train: - {:.3f}] [acc - {:.4f}] [auc - {:.4f}]".format(epoch+1, EPOCHS, loss, acc, auc))
+    loss, acc, auc = MD.valid_fn(model, valid_dataloader, criterion, device)
+    print("[epoch - {}/{}] [valid: - {:.3f}] [acc - {:.4f}] [auc - {:.4f}]\n".format(epoch+1, EPOCHS, loss, acc, auc))
+    if auc > best_auc:
+        best_auc = auc
+        step = 0
+        torch.save(model.state_dict(), folder + "/sakt_model.pt")
+    else:
+        step += 1
+        if step >= max_steps:
+            break
+torch.save(model.state_dict(), folder + "/sakt_model_final.pt")
